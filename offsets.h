10bc43a8ba889ac639f1b9889bc57b39ac2e0628
boyo3473 <141408370+boyo3473@users.noreply.github.com>
2024-10-05 22:28:19 +0100

    Create offsets.h

---
+++ offsets.h
@@ -0,0 +1,14 @@
+namespace offsets 
+{
+    constexpr std::ptrdiff_t dwEntityList = 0x19BBCC8;
+    constexpr std::ptrdiff_t dwLocalPlayerPawn = 0x1823128;
+    constexpr std::ptrdiff_t dwViewAngles = 0x1A27C50;
+    constexpr std::ptrdiff_t dwViewMatrix = 0x1A1DDE0;
+    constexpr std::ptrdiff_t jump = 0x181B0D0;
+    constexpr std::ptrdiff_t m_iIDEntIndex = 0x1450; 
+    constexpr std::ptrdiff_t m_iTeamNum = 0x3E3; 
+    constexpr std::ptrdiff_t m_iHealth = 0x344; 
+    constexpr std::ptrdiff_t fFlags = 0x3EC;
+    constexpr std::ptrdiff_t vecOrigin = 0x131C;
+    constexpr std::ptrdiff_t m_flFlashDuration = 0x1404;
+}
